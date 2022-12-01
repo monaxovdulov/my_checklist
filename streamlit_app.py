cd98811e61635f136f034815a0fc7ddd8d3b6a54
Roman <35300872+monaxovdulov@users.noreply.github.com>
2022-12-02 01:10:50 +0300

    add agree

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ import streamlit as st
 dice_type = st.sidebar.selectbox('Type of dice', ('coin', 'tetrahedron', 'nonagon', 'dice'))
 st.write("Hello word!")
 st.write(dice_type)
+
+agree = st.checkbox('I agree')
+
+if agree:
+    st.write('Great!')
